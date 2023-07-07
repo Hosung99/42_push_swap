c2c478a4d5bb2d01dfa44833691707ba87ed0cf3
seongho <seongho@seonghoui-MacBookPro.local>
2023-07-07 13:46:43 +0900

    update init함수

---
+++ push_swap_init.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap_init.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seongho <seongho@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/07 13:25:29 by seongho           #+#    #+#             */
+/*   Updated: 2023/07/07 13:46:23 by seongho          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	get_min_list(t_list **stack_a)
+{
+	int	min;
+
+	min = -2147483648;
+	
+}
+
+void	init(t_list **stack_a, t_list **stack_b)
+{
+	t_list	*temp;
+	
+	temp = (*stack_a);
+	while (temp->next)
+	{
+		
+	}
+}
